7eee06d0201c2a9482ebbcb1a2f96b82f9abbbc3
carslaw <carslaw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-25 10:54:17 +0000

    New option in time.average.R (start.date), no longer averages date to help with post-merging; minor change in calendar.plot.Rd
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/openair@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/openair/R/time.average.R
+++ pkg/openair/R/time.average.R
@@ -5,11 +5,13 @@
 ## Note that "period" uses those defined in cut.POSIXct e.g. "days", "5 days", and
 ## is therefore extremely flexible.
 ## The function will also work with mutiple sites.
+## start.date helps to get data frame into nice time sequence
 ## NOTE - will only return numeric data apart from site name
 
 time.average <- function(mydata, period = "day", data.thresh = 0,
-                         statistic = "mean", percentile = 95) {
+                         statistic = "mean", percentile = 95, start.date = NA) {
     library(plyr)
+
     percentile <- percentile / 100
     if (percentile < 0 | percentile > 100) stop("Percentile range outside 0-100")
     if (data.thresh < 0 | data.thresh > 100) stop("Data capture range outside 0-100")
@@ -23,12 +25,18 @@ time.average <- function(mydata, period = "day", data.thresh = 0,
     if (statistic == "frequency") form <- "length(na.omit(x))"
     if (statistic == "percentile") form <- "quantile(x, probs = percentile, na.rm = TRUE)"
 
-   
-    calc.mean <- function(mydata) { ## function to calculate means
+
+    calc.mean <- function(mydata, start.date) { ## function to calculate means
 
          ## pad out missing data
-        mydata <- date.pad(mydata)   
-        
+        mydata <- date.pad(mydata)
+
+        ## start from a particular time, if given
+        if (!is.na(start.date)) {
+            firstLine <- data.frame(date = as.POSIXct(start.date, "GMT"))
+            mydata <- rbind.fill(firstLine, mydata)
+        }
+
         if ("wd" %in% names(mydata)) {
             mydata$u <- sin(2 * pi * mydata$wd / 360)
             mydata$v <- cos(2 * pi * mydata$wd / 360)
@@ -36,29 +44,26 @@ time.average <- function(mydata, period = "day", data.thresh = 0,
 
         ## cut into sections dependent on period
         mydata$cuts <- cut(mydata$date, period)
-        
+
         newMean <- function(x, data.thresh) {
             ## calculate mean only if above data capture threshold
             if (length(na.omit(x)) >= round(length(x) * data.thresh / 100)) {
-                res <- eval(parse(text = form)) 
+                res <- eval(parse(text = form))
             } else {
                 res <- NA
             }
             res
         }
-      
+
         dailymet <- aggregate(mydata[ , sapply(mydata, class) %in% c("numeric", "integer"),
                          drop = FALSE], list(date = mydata$cuts), newMean,
                               data.thresh = data.thresh)
 
-        ## mean date as numeric, to get mean date rather than start date
-        mydata$MeanDate <- as.numeric(mydata$date)
-        meanDates <-  tapply(mydata$MeanDate, mydata$cuts, mean)
-        dailymet$date <- as.POSIXct(meanDates, origin = "1970-01-01", "GMT")     
-   
+        dailymet$date <- as.POSIXct(dailymet$date, "GMT")
+
         if ("wd" %in% names(mydata)) {
-            ## mean wd 
-            dailymet <- within(dailymet, wd <- as.vector(atan2(u, v) * 360 /2 / pi))
+            ## mean wd
+            dailymet <- within(dailymet, wd <- as.vector(atan2(u, v) * 360 / 2 / pi))
 
             ## correct for negative wind directions
             ids <- which(dailymet$wd < 0)  ## ids where wd < 0
@@ -68,17 +73,17 @@ time.average <- function(mydata, period = "day", data.thresh = 0,
         }
 
          if ("site" %in% names(mydata)) dailymet$site <- mydata$site[1]
-        
+
         dailymet
     }
 
     ## split if several sites
     if ("site" %in% names(mydata)) { ## split by site
         mydata$site <- factor(mydata$site)
-        mydata <- ddply(mydata, .(site), calc.mean)
+        mydata <- ddply(mydata, .(site), calc.mean, start.date)
         mydata
     } else {
-        mydata <- calc.mean(mydata)
+        mydata <- calc.mean(mydata, start.date)
         mydata
     }
 }
