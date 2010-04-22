d12d29456371371aa605a50c9ff351eaad2212ce
karl <karl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-22 18:43:01 +0000

    update to file structure
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/openair@111 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/openair/R/import.ADMS.met.R
+++ pkg/openair/R/import.ADMS.met.R
@@ -1,37 +1,92 @@
 
 import.ADMS.met <- function(...) { import.adms.met(...) }
 
-import.adms.met <- function(filename = file.choose()) {
-    met <- read.csv(filename, nrows = 1, header = TRUE)
-    variables <- met[1,1]
-    met <- read.csv(filename, nrows= variables + 1, header = TRUE)
-    variables <- met[2:(variables +1),]
+import.adms.met <- function(file = file.choose()
+         , drop.case=TRUE, drop.input.dates=TRUE
+         , test.file.structure = TRUE
+         , simplify.names=TRUE
+         , ...
+) {
+    met <- read.csv(file, nrows = 2, header = FALSE)
 
-    met <- read.csv(filename, skip = length(variables) + 3, header = FALSE,
+    test <- as.character(met[1,1])[1]
+    variables <- suppressWarnings(as.numeric(as.character(met[2,1])))
+
+    #file structure test 1
+    allowed.formats <- c("VARIABLES:")
+    if(test.file.structure){
+      #check a number
+      if( !test %in% allowed.formats | is.na(variables) ) {
+        stop("File not recognised ADMS.met structure\n       [please contact openair if valid]"
+          , call. = FALSE
+        )
+      }
+    }
+
+    met <- read.csv(file, nrows= variables + 2, header = TRUE)
+
+    test <- as.character(met[variables+2,1])[1]
+    variables <- as.character(met[2:(variables +1),])
+
+    #file structure test 2
+    if(test.file.structure){
+      #check data where should be
+      if( test!="DATA:" ) {
+        stop("File not recognised ADMS.met structure\n       [please contact openair if valid]"
+          , call. = FALSE
+        )
+      }
+    }
+
+    met <- read.csv(file, skip = length(variables) + 3, header = FALSE,
                     na.strings = c("-999", "-999.0"))
-
-    names(met) <- tolower(variables)
-    met$date <- paste(met$year, met$tday, met$thour, sep = "-")
+    ## remove variables where all row are NA
+    met <- met[ , sapply(met, function(x) !all(is.na(x)))]
+    names(met) <- make.names(variables, unique=TRUE) 
+    met <- cbind(date = paste(met$YEAR, met$TDAY, met$THOUR, sep = "-"), met)
     met$date <- as.POSIXct(strptime(met$date, format = "%Y-%j-%H"), "GMT")
 
-    if ("t0c" %in% names(met)) {
-        id <- which(names(met) == "t0c")
+    if(drop.input.dates){
+      met <- met[,!names(met) %in% c("YEAR", "TDAY", "THOUR")]
+    }
+
+    if(simplify.names){
+      if ("T0C" %in% names(met)) {
+        id <- which(names(met) == "T0C")
         names(met)[id] <- "temp"
-    }
-
-    if ("u" %in% names(met)) {
-        id <- which(names(met) == "u")
+      }
+      if ("U" %in% names(met)) {
+        id <- which(names(met) == "U")
         names(met)[id] <- "ws"
-    }
-
-    if ("phi" %in% names(met)) {
-        id <- which(names(met) == "phi")
+      }
+      if ("PHI" %in% names(met)) {
+        id <- which(names(met) == "PHI")
         names(met)[id] <- "wd"
+      }
     }
 
-    met[] <- lapply(met, function(x){replace(x, x == -999, NA)})
-     ## remove variables where all are NA
-    met <- met[ , sapply(met, function(x) !all(is.na(x)))]
-    met
+    if(drop.case){
+      names(met) <- tolower(names(met))
+    } 
+   
 
+   #tidy for export
+    met[] <- lapply(met, function(x){replace(x, x == -999, NA)})
+    ##error handling for bad days
+    ids <- which(is.na(met$date))
+    if (length(ids) > 0) {
+      if(length(ids)==nrow(met)) {
+        stop("Invalid date (and time) format requested\n       [compare openair import settings and data structure]"
+          , call. = FALSE
+        )
+      }
+      met <- met[-ids, ]
+      reply <- paste("Missing dates detected, removing", length(ids), "line", sep=" ")
+      if(length(ids)>1) { reply <- paste(reply,"s",sep="") }
+      warning(reply, call. = FALSE)
+    }
+
+    #report met stucture   
+    print(unlist(sapply(met, class)))
+    met
 }
