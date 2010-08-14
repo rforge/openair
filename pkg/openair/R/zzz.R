efa282e0f1af8166e09aba1ee7154768e0a534f3
karl <karl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-14 06:02:04 +0000

    Win compile problem returned tcltk to namespace load
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/openair@207 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/openair/R/zzz.R
+++ pkg/openair/R/zzz.R
@@ -13,7 +13,7 @@ ins.pcks <- .packages(all.available = TRUE)
 open.test <- 0
 
 #packages loaded as library
-req.pcks <- c("proto", "grid", "plyr", "reshape", "splines", "survival", "lattice", "RColorBrewer", "tcltk")
+req.pcks <- c("proto", "grid", "plyr", "reshape", "splines", "survival", "lattice", "RColorBrewer")
     for (pck in req.pcks) {
         if(pck %in% ins.pcks) {
             suppressWarnings(require(pck, character.only = TRUE, quietly = TRUE, warn.conflicts = FALSE))
@@ -26,7 +26,7 @@ req.pcks <- c("proto", "grid", "plyr", "reshape", "splines", "survival", "lattic
     }    
 
 #rest loaded as namespaces in openair
-req.pcks <- all.pcks[!all.pcks %in% c("proto", "tcltk")]
+req.pcks <- all.pcks[!all.pcks %in% "proto"]
    for (pck in req.pcks) {
        if(pck %in% ins.pcks & any(packageHasNamespace(pck, .libPaths()))) {
            suppressWarnings(namespaceImport("openair", pck))
