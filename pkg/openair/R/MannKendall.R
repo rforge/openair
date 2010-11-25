d9085a555534b2022453a16ce665a94787a32d57
carslaw <carslaw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-25 08:59:14 +0000

    new normalise option in polarPlot and bug fix aggregate in MannKendall
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/openair@282 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/openair/R/MannKendall.R
+++ pkg/openair/R/MannKendall.R
@@ -172,16 +172,16 @@ MannKendall <- function(mydata,
 
     ## calculate percentage changes in slope and uncertainties
     ## need start and end dates (in days) to work out concentrations at those points
-    ## percentage change defind as 100.(C.end/C.start -1) / (Date.end = Date.start)
+    ## percentage change defind as 100.(C.end/C.start -1) / (Date.end - Date.start)
 
     start <- aggregate(split.data, list(variable = split.data$cond), function (x) head(x, 1))
     end <- aggregate(split.data, list(variable = split.data$cond), function (x) tail(x, 1))
     percent.change <- merge(start, end, by = "variable", suffixes = c(".start", ".end"))
 
     percent.change <- transform(percent.change, slope.percent = 100 * 365 *
-                                ((slope.start * date.end / 365 + intercept.start) /
-                                 (slope.start * date.start / 365 + intercept.start) - 1) /
-                                (date.end - date.start))
+                                ((slope.start * as.numeric(date.end) / 365 + intercept.start) /
+                                 (slope.start * as.numeric(date.start) / 365 + intercept.start) - 1) /
+                                (as.numeric(date.end) - as.numeric(date.start)))
 
     percent.change <- transform(percent.change, lower.percent = slope.percent / slope.start * lower.start,
                                 upper.percent = slope.percent / slope.start * upper.start)
