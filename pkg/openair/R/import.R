0b1b16bbc69b30fb4b21f35c53e4fdece7d0e582
carslaw <carslaw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-02 16:14:23 +0000

    fix tab-file import.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/openair@600 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/openair/R/import.R
+++ pkg/openair/R/import.R
@@ -98,7 +98,7 @@ import <- function (file = file.choose(), file.type = "csv", sep = ",", header.a
                     correct.time = NULL, ...)
 {
 
-    if (file.type == "csv") sep <- "," else sep <- "\t"
+    if (file.type == "csv") sep <- "," else sep <- ""
 
     if (!missing(sep)) sep <- sep
 
