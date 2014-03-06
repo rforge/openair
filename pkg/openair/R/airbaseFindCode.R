c18f7fbd73cb27bd7b45d7f0048f5feb18b967ab
carslaw <carslaw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-03-06 12:03:35 +0000

    update airbaseFindCode to allow cities to be selected
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/openair@823 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/openair/R/airbaseFindCode.R
+++ pkg/openair/R/airbaseFindCode.R
@@ -14,6 +14,24 @@
 ##' @param local.code A character or vector of characters representing
 ##' the local site code. For example \dQuote{MY1} is the UK code for
 ##' Marylebone Road.
+##' @param city A city name to search --- using character matching
+##' (\code{grep}). The search string can be upper or lower case
+##' e.g. \code{city = "london"}. It is also possible to use
+##' regular expressions. For example, to extract sites in Copenhagen or
+##' Barcelona use \code{city = "copenhagen|barcelona"}. Note that by
+##' default any matching characters are returned, so \code{city =
+##' "london"} would also return Londonderry (Northern Ireland).
+##'
+##' Regular expression searches are very powerfull and potentially
+##' complicated. However there are a few useful tips. To match the
+##' beginning of a name use \sQuote{^}. So \code{city = "^london"}
+##' would return London and Londonderry (both begin with
+##' \sQuote{london}. To match the end of a name use \sQuote{$}, so
+##' \code{city = "london$"} would just return London but not
+##' Londonderry.
+##'
+##' The cities chosen are printed to screen to make it easy to check
+##' (and refine) the selected sites.
 ##' @param lat The latitude range to select in the form c(lower, upper).
 ##' @param lon The longitude range to select in the form c(lower, upper).
 ##' @return A vector of airbase site codes that can be used directly
@@ -33,7 +51,7 @@ airbaseFindCode <- function(country = c("AL", "AT", "BA", "BE", "BG", "CH", "CY"
                                 "NL", "NO", "PL", "PT", "RO", "RS", "SE", "SI", "SK", "TR"),
                             site.type = c("background", "traffic", "industrial", "unknown"),
                             area.type = c("rural", "urban", "suburban", "unknown"), 
-                            local.code = NA, 
+                            local.code = NA, city = NA, 
                             lat = c(-90, 90), lon = c(-180, 180)) {
 
 
@@ -50,6 +68,16 @@ airbaseFindCode <- function(country = c("AL", "AT", "BA", "BE", "BG", "CH", "CY"
 
     ## site type
     res <- res[toupper(res$site.type) %in% toupper(site.type) , ]
+
+    ## city, using grep
+    if (!is.na(city)) {
+        id <- grep(city, res$city, ignore.case = TRUE)
+        cities <- data.frame(code = res$code[id], city = res$city[id])
+        print(cities)
+        res <- res[id, ]
+        
+        
+    }
 
     ## area type
     res <- res[toupper(res$station_type_of_area) %in% toupper(area.type) , ]
